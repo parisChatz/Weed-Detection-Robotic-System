30e3964a5322582b4a75eadc1c57e08a18dc91f7
Paris Chatz <41471360+parisChatz@users.noreply.github.com>
2019-10-08 16:19:10 +0100

    Add files via upload

--- wall_avoidance_package/scripts/wall_avoidance_node.py
+++ wall_avoidance_package/scripts/wall_avoidance_node.py
@@ -5,27 +5,32 @@ from sensor_msgs.msg import LaserScan
 import numpy as np
 
 
-threshold = 3 #THRESHOLD value for laser scan.
+threshold = 1.5 #THRESHOLD value for laser scan.
 
 class wall_avoid:
 
-    def __init__(self):
-        self.publisher = rospy.Publisher('/thorvald_001/turtle1/cmd_vel', Twist, queue_size=10)
-        rospy.Subscriber("/thorvald_001/scan", LaserScan , self.LaserScanProcess)
+	def __init__(self,robot):
+		self.publisher = rospy.Publisher('/'+robot+'/turtle1/cmd_vel', Twist, queue_size=10)
+		rospy.Subscriber("/"+robot+"/scan", LaserScan , self.LaserScanProcess)
 
-    def LaserScanProcess(self, data):
-        ranges = np.array(data.ranges)
-        min_left = min(ranges[0:360]) 
-        min_right = min(ranges[360:720])
-        command = Twist()
-        command.linear.x = 1.0
-        if min_left < threshold:
-            command.angular.z = 1.0
-        if min_right < threshold:
-            command.angular.z = -1.0
-        self.publisher.publish(command)
+
+
+	def LaserScanProcess(self, data):
+		ranges = np.array(data.ranges)
+		min_left = min(ranges[0:360]) 
+		min_right = min(ranges[360:720])
+		command = Twist()
+		command.linear.x = 1.0
+		if min_left < threshold:
+			command.angular.z = 1.0
+		if min_right < threshold:
+			command.angular.z = -1.0
+		self.publisher.publish(command)
+
+
 
 if __name__ == '__main__':
     rospy.init_node('wall_avoidance_node')
-    wall_avoid()
+    wall_avoid("thorvald_001")
+    wall_avoid("thorvald_002")
     rospy.spin()
